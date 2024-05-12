a2c20005405e177368cfeaf64a94e11d892571b7
shihabpalekar <shihabpalekar23@gmail.com>
2024-05-12 16:24:56 +0530

    home page UI

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,58 @@
-const Home = () => {
-    return(
-        <div>Home</div>
-    )
-}
+import { useEffect, useState } from "react";
+import { dummyData } from "../../dummyData";
+import "./style.css";
+import { useNavigate } from "react-router";
 
-export default Home
+const Home = () => {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setData(dummyData);
+  }, []);
+  return (
+    <div className="container">
+      <div className="data-table">
+        <div className="heading-row">
+          <div className="heading-cell">Company / Issue date</div>
+          <div className="heading-cell">Issue size</div>
+          <div className="heading-cell">Price range</div>
+          <div className="heading-cell">Min invest/qty</div>
+        </div>
+        {data &&
+          data.map((item) => (
+            <div
+              key={item.id}
+              className="data-row"
+              onClick={() => navigate(`/details/${item.id}`)}
+            >
+              <div className="data-cell">
+                <img
+                  className="logo"
+                  src={item.logoUrl}
+                  width={"50px"}
+                  height={"50px"}
+                />
+                <div>
+                  <div className="main-text">{item.name}</div>
+                  <div className="sub-text">{item.issueDate}</div>
+                </div>
+              </div>
+              <div className="data-cell main-text">₹{item.issueSize}</div>
+              <div className="data-cell main-text">₹{item.priceRange}</div>
+              <div className="data-cell ">
+                <div>
+                  <div className="main-text">₹{item.minInvestAmount}</div>
+                  <div className="sub-text">{item.minInvestQty}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
